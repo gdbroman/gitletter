45b1775a62efef3078688798d3baa01692f5305e
Gustaf Broman <gustaf.broman@tink.com>
2022-06-14 22:55:10 +0200

    fix: fouc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,32 @@
 import "../styles/globals.css";
 
-import { CacheProvider } from "@emotion/react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import { FC } from "react";
-import { JssProvider } from "react-jss";
+import { FC, useEffect } from "react";
 
 import { NewsletterProvider } from "../contexts/NewsletterContext";
 import theme from "../styles/theme";
-import createEmotionCache from "../util/createEmotionCache";
 
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
 
-type AppPropsExtended = {
-  emotionCache?: typeof clientSideEmotionCache;
-} & AppProps;
-
-const App: FC<AppPropsExtended> = ({
-  Component,
-  pageProps,
-  emotionCache = clientSideEmotionCache,
-}) => (
-  <JssProvider>
-    <CacheProvider value={emotionCache}>
-      <SessionProvider session={pageProps.session}>
-        <NewsletterProvider>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </NewsletterProvider>
-      </SessionProvider>
-    </CacheProvider>
-  </JssProvider>
-);
+  return (
+    <SessionProvider session={pageProps.session}>
+      <NewsletterProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </NewsletterProvider>
+    </SessionProvider>
+  );
+};
 
 export default App;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,8 @@
-import createEmotionServer from "@emotion/server/create-instance";
+import { ServerStyleSheets } from "@mui/styles";
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { Children } from "react";
 
 import theme from "../styles/theme";
-import createEmotionCache from "../util/createEmotionCache";
 
 export default class MyDocument extends Document {
   render() {
@@ -28,8 +28,6 @@ export default class MyDocument extends Document {
             href="/favicon-16x16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
-          {/* Inject MUI styles first to match with the prepend: true configuration. */}
-          {(this.props as any)?.emotionStyleTags}
         </Head>
         <body>
           <Main />
@@ -41,60 +39,25 @@ export default class MyDocument extends Document {
 }
 
 // `getInitialProps` belongs to `_document` (instead of `_app`),
-// it's compatible with static-site generation (SSG).
+// it's compatible with server-side generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
   const originalRenderPage = ctx.renderPage;
-
-  // You can consider sharing the same Emotion cache between all the SSR requests to speed up performance.
-  // However, be aware that it can have global side effects.
-  const cache = createEmotionCache();
-  const { extractCriticalToChunks } = createEmotionServer(cache);
 
   ctx.renderPage = () =>
     originalRenderPage({
-      enhanceApp: (App: any) =>
-        function EnhanceApp(props) {
-          return <App emotionCache={cache} {...props} />;
-        },
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
     });
 
   const initialProps = await Document.getInitialProps(ctx);
-  // This is important. It prevents Emotion to render invalid HTML.
-  // See https://github.com/mui/material-ui/issues/26561#issuecomment-855286153
-  const emotionStyles = extractCriticalToChunks(initialProps.html);
-  const emotionStyleTags = emotionStyles.styles.map((style) => (
-    <style
-      data-emotion={`${style.key} ${style.ids.join(" ")}`}
-      key={style.key}
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: style.css }}
-    />
-  ));
 
   return {
     ...initialProps,
-    emotionStyleTags,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
   };
 };
