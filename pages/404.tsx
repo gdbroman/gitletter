c02cf85ff61b234efa5d3014cb7eae80bf33a349
gdbroman <99gustaf@gmail.com>
2022-06-15 22:13:27 +0200

    feat: 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,24 @@
+import { Box, Button, Typography } from "@mui/material";
+import Link from "next/link";
+import { FC } from "react";
+
+import Layout from "../components/Layout";
+
+const FourOhFour: FC = () => (
+  <Layout>
+    <main>
+      <Box maxWidth={780} mx="auto" my="64px" textAlign="center">
+        <Typography variant="h1" fontWeight="bold" mb="32px">
+          404 Not found
+        </Typography>
+        <Link href="/" passHref>
+          <Button variant="contained" size="large">
+            Return home
+          </Button>
+        </Link>
+      </Box>
+    </main>
+  </Layout>
+);
+
+export default FourOhFour;
