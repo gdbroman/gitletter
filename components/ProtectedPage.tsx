bc1b26c9229dda8d6e94c8b658e497d55ba41d31
gdbroman <99gustaf@gmail.com>
2022-06-15 22:24:45 +0200

    fix: protected routes

--- components/ProtectedPage.tsx
+++ components/ProtectedPage.tsx
@@ -1,16 +1,20 @@
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
 export const ProtectedPage = ({ children }) => {
   const router = useRouter();
   const { status } = useSession();
+  const loading = useMemo(() => status === "loading", [status]);
+  const unauthenticated = useMemo(() => status === "unauthenticated", [status]);
 
   useEffect(() => {
-    if (status === "unauthenticated") {
+    if (unauthenticated) {
       router.push("/");
     }
-  }, [router, status]);
+  }, [router, unauthenticated]);
+
+  if (loading || unauthenticated) return null;
 
   return <>{children}</>;
 };
