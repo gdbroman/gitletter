29e3cae5a80ff8bad53cabfa1e441738bbc0d941
gdbroman <99gustaf@gmail.com>
2022-06-19 15:24:20 +0200

    cleaner gh alert

--- components/settings/GithubIntegrationCard.tsx
+++ components/settings/GithubIntegrationCard.tsx
@@ -1,6 +1,8 @@
+import styled from "@emotion/styled";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LoadingButton from "@mui/lab/LoadingButton";
 import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
@@ -16,6 +18,15 @@ import { deleteIntegration, updateIntegration } from "../../util/githubClient";
 import { useToggle } from "../../util/hooks";
 import { Nullable } from "../../util/types";
 import { LoadingButtonWithBlackSpinner } from "../Header/HeaderNavRight";
+
+const StyledAlert = styled(Alert)`
+  .MuiAlert-message {
+    width: 100%;
+  }
+  .MuiAlert-icon {
+    display: none;
+  }
+`;
 
 type Props = {
   githubIntegration: GithubIntegration;
@@ -276,30 +287,32 @@ const GithubIntegrationSettingsCard: FC<GithubIntegrationSettingsCardProps> = ({
 
 const GithubIntegrationConnectionCard: FC = () => (
   <Card variant="outlined">
-    <Alert severity="info" icon={<GitHubIcon />}>
-      GitHub integration
-    </Alert>
-    <Box
-      display="flex"
-      flexDirection="column"
-      gap={3}
-      padding={2}
-      paddingTop={4}
+    <StyledAlert
+      severity="info"
+      style={{ display: "flex", alignItems: "center" }}
     >
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Typography variant="caption" color="gray">
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        padding={1}
+      >
+        <Box>
+          <AlertTitle style={{ margin: 0 }}>
+            GitHub integration required
+          </AlertTitle>
           Setup takes less than a minute
-        </Typography>
-        <Box display="flex" columnGap={1}>
-          <Button
-            variant="contained"
-            color="primary"
-            href={process.env.GITHUB_APP_URL}
-          >
-            Connect your repo
-          </Button>
         </Box>
+        <Button
+          variant="contained"
+          size="medium"
+          color="primary"
+          href={process.env.GITHUB_APP_URL}
+          startIcon={<GitHubIcon />}
+        >
+          Connect your repo
+        </Button>
       </Box>
-    </Box>
+    </StyledAlert>
   </Card>
 );
