b3bc4fc0bb8060e50583727ad78f4f9a549a842e
gdbroman <99gustaf@gmail.com>
2022-06-19 15:52:07 +0200

    fix header padding

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import Box from "@mui/material/Box";
 import { FC } from "react";
 
 import { GitLetterLogo } from "./GitLetterLogo";
@@ -6,7 +7,6 @@ import { HeaderNavRight } from "./HeaderNavRight";
 
 const StyledHeader = styled.header`
   display: flex;
-  height: 70px;
   padding: 0 16px;
   align-items: center;
 `;
@@ -14,13 +14,16 @@ const StyledHeader = styled.header`
 const StyledNav = styled.nav`
   flex: 1;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `;
 
 export const Header: FC = () => (
   <StyledHeader>
     <StyledNav>
-      <GitLetterLogo />
+      <Box paddingY={2}>
+        <GitLetterLogo />
+      </Box>
       <HeaderNavRight />
     </StyledNav>
   </StyledHeader>
