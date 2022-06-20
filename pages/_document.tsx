1e5a793ee74388a6515aa79e62ef6953acf0727b
Gustaf Broman <gustaf.broman@tink.com>
2022-06-20 23:17:38 +0200

    chore: favicon folder

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,21 +13,21 @@ export default class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href="/favicon/apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href="/favicon/favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="/favicon/favicon-16x16.png"
           />
-          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
         </Head>
         <body>
           <Main />
