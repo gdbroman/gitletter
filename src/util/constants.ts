475eea9f6bfcf478a82650957c53722dd5b09470
Gustaf Broman <gustaf.broman@tink.com>
2022-06-27 20:47:03 +0200

    sharpen

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,7 +1,6 @@
 export const siteTitle = "GitLetter";
 export const siteTagline = "Start a newsletter using GitHub";
-export const shortDescription =
-  "A newsletter tool that writes directly to your repo.";
+export const shortDescription = "A newsletter tool that writes to your repo.";
 export const siteDescription =
   "Keep all your writing in one repository. Integrate with your existing blog.";
 export const siteURL = "https://gitletter.co";
