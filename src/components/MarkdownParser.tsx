3a498bab1bef3d5522d5924bfcb4077193571472
Gustaf Broman <gustaf.broman@tink.com>
2022-07-10 18:36:05 +0200

    auto embed tweets

--- src/components/MarkdownParser.tsx
+++ src/components/MarkdownParser.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import ReactMarkdown from "react-markdown";
+import { TwitterTweetEmbed } from "react-twitter-embed";
 import gfm from "remark-gfm";
 
 type Props = {
@@ -7,5 +8,27 @@ type Props = {
 };
 
 export const MarkdownParser: FC<Props> = ({ children }) => (
-  <ReactMarkdown remarkPlugins={[gfm]} children={children} />
+  <ReactMarkdown
+    remarkPlugins={[gfm]}
+    children={children}
+    components={{
+      a: (props) => {
+        const isTwitter = props.href.startsWith("https://twitter.com/");
+
+        if (isTwitter) {
+          const tweetId = props.href.split("/")[5];
+
+          console.log("tweetId", tweetId);
+
+          return <TwitterTweetEmbed tweetId={tweetId} />;
+        }
+
+        return (
+          <a href={props.href} target="_blank" rel="noreferrer">
+            {props.children}
+          </a>
+        );
+      },
+    }}
+  />
 );
