51ea411a8690d59d2a0966d4da5429db6fb1e830
Gustaf Broman <gustaf.broman@tink.com>
2022-08-27 19:47:19 +0200

    capitalize tagline

--- util/constants.ts
+++ util/constants.ts
@@ -1,5 +1,5 @@
 export const siteTitle = "GitLetter";
-export const siteTagline = "Add a newsletter to your markdown blog";
+export const siteTagline = "Add a Newsletter to Your Markdown Blog";
 export const siteDescription =
   "Keep all your writing on GitHub. No more copy pasting.";
 export const siteDomain = "gitletter.co";
