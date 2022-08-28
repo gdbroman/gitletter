e4905a6717afe78e73c427589bad90133bba9c68
Gustaf Broman <gustaf.broman@tink.com>
2022-08-28 11:11:58 +0200

    cleaner tabs design

--- src/containers/dashboard/NavBar.tsx
+++ src/containers/dashboard/NavBar.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
 import PeopleIcon from "@mui/icons-material/People";
 import SendIcon from "@mui/icons-material/Send";
@@ -12,15 +13,45 @@ import { FC } from "react";
 import { useAppHref } from "../../../util/hooks/useAppHref";
 import { NavBarCta } from "./NavBarCta";
 
-const tabs = ["/", "sent", "subscribers", "settings"];
+// Style tab with hover effect
+const StyledTab = styled(Tab)`
+  && {
+    height: 30px;
+    min-height: 30px;
+    margin: 9px 8px 9px 0;
+    padding: 0 8px;
+    border-radius: 6px;
+    &:hover {
+      background-color: #eeeeee;
+    }
+  }
+`;
+
+const tabs = {
+  "": {
+    label: "Drafts",
+    icon: <InsertDriveFileIcon />,
+  },
+  sent: {
+    label: "Sent",
+    icon: <SendIcon />,
+  },
+  subscribers: {
+    label: "Subscribers",
+    icon: <PeopleIcon />,
+  },
+  settings: {
+    label: "Settings",
+    icon: <SettingsIcon />,
+  },
+};
 
 export const NavBar: FC = () => {
   const router = useRouter();
   const appHref = useAppHref();
   const newsletterId = router.query.newsletterId as string;
 
-  const path = router.pathname.split("/")[3] ?? "/";
-  console.log(path);
+  const path = router.pathname.split("/")[3] ?? "";
 
   return (
     <Box
@@ -29,64 +60,29 @@ export const NavBar: FC = () => {
       justifyContent="space-between"
       sx={{ borderBottom: 1, borderColor: "divider" }}
     >
-      <Tabs
-        value={tabs.indexOf(path)}
-        variant="scrollable"
-        scrollButtons="auto"
-      >
-        <Link href={appHref} passHref>
-          <Tab
-            label={
-              <Box display="flex" alignItems="center" gap={1}>
-                <InsertDriveFileIcon fontSize="small" />
-                Drafts
-              </Box>
-            }
-            {...a11yProps(0)}
-          />
-        </Link>
-        <Link href={`${appHref}/sent`} passHref>
-          <Tab
-            label={
-              <Box display="flex" alignItems="center" gap={1}>
-                <SendIcon fontSize="small" />
-                Sent
-              </Box>
-            }
-            {...a11yProps(0)}
-          />
-        </Link>
-        <Link href={`${appHref}/subscribers`} passHref>
-          <Tab
-            label={
-              <Box display="flex" alignItems="center" gap={1}>
-                <PeopleIcon fontSize="small" />
-                Subscribers
-              </Box>
-            }
-            {...a11yProps(1)}
-          />
-        </Link>
-        <Link href={`${appHref}/settings`} passHref>
-          <Tab
-            label={
-              <Box display="flex" alignItems="center" gap={1}>
-                <SettingsIcon fontSize="small" />
-                Settings
-              </Box>
-            }
-            {...a11yProps(2)}
-          />
-        </Link>
+      <Tabs value={Object.keys(tabs).indexOf(path)} variant="scrollable">
+        {Object.entries(tabs).map(([path, { label, icon }]) => (
+          <Link href={`${appHref}/${path}`} passHref key={path}>
+            <StyledTab
+              label={
+                <Box display="flex" alignItems="center" gap={1}>
+                  {icon}
+                  {label}
+                </Box>
+              }
+              {...a11yProps(path)}
+            />
+          </Link>
+        ))}
       </Tabs>
       <NavBarCta newsletterId={newsletterId} />
     </Box>
   );
 };
 
-const a11yProps = (index: number) => {
+const a11yProps = (path: string) => {
   return {
-    id: `dashboard-tab-${index}`,
-    "aria-controls": `dashboard-tabpanel-${index}`,
+    id: `dashboard-tab-${path}`,
+    "aria-controls": `dashboard-tabpanel-${path}`,
   };
 };
