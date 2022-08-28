4ba5a691fd1c34ca75ba7c5aa0e9faf9e95f118d
Gustaf Broman <gustaf.broman@tink.com>
2022-08-28 17:41:33 +0200

    fix title positioning

--- src/containers/dashboard/Dashboard.tsx
+++ src/containers/dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import { FC, ReactNode } from "react";
 
 import { TabsNavBar } from "./TabsNavBar";
@@ -10,7 +11,9 @@ export type DashboardProps = {
 
 export const Dashboard: FC<DashboardProps> = ({ title, children }) => (
   <main>
-    <h1>{title}</h1>
+    <Typography variant="h3" fontWeight={700} mb={3} mt={4}>
+      {title}
+    </Typography>
     <TabsNavBar />
     <Box role="tabpanel" my={3}>
       {children}
