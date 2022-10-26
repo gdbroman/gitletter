025701ffb1b160da1946e8ed9363c8601bf0353b
gdbroman <99gustaf@gmail.com>
2022-10-26 20:33:25 +0200

    fix ugly loading btn

--- src/components/LoadingButton.tsx
+++ src/components/LoadingButton.tsx
@@ -3,34 +3,22 @@ import MuiButton from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 import { ComponentProps, FC, ReactNode } from "react";
 
-import { useThemeContext } from "../contexts/theme";
-
 type Props = {
   children: ReactNode;
   loading?: boolean;
 } & ComponentProps<typeof MuiButton>;
 
-export const LoadingButton: FC<Props> = ({ children, loading, ...props }) => {
-  const { theme } = useThemeContext();
-
-  return (
-    <>
-      {loading ? (
-        <MuiLoadingButton
-          loading
-          {...props}
-          style={{
-            color: theme.palette.primary.main,
-          }}
-        >
-          {children}
-        </MuiLoadingButton>
-      ) : (
-        <MuiButton {...props}>{children}</MuiButton>
-      )}
-    </>
-  );
-};
+export const LoadingButton: FC<Props> = ({ children, loading, ...props }) => (
+  <>
+    {loading ? (
+      <MuiLoadingButton loading {...props}>
+        {children}
+      </MuiLoadingButton>
+    ) : (
+      <MuiButton {...props}>{children}</MuiButton>
+    )}
+  </>
+);
 
 export const LoadingButtonWithBlackSpinner = styled(LoadingButton)`
   &:disabled .MuiLoadingButton-loadingIndicator {
