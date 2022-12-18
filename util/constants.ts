3ac67375a9d54d4d16c01c45136b511aba211e6e
gdbroman <99gustaf@gmail.com>
2022-12-19 00:11:47 +0100

    update desc

--- util/constants.ts
+++ util/constants.ts
@@ -1,7 +1,8 @@
 export const siteTitle = "GitLetter";
 export const siteH1 = "The Newsletter Tool That Deploys to GitHub";
 export const siteTagline = "Become Your Own Publisher";
-export const siteDescription = "Keep all your writing in one place.";
+export const siteDescription =
+  "Keep your stack. Keep your writing in one place.";
 export const siteDomain = "gitletter.co";
 
 export const maxEmailAddressLength = 64;
