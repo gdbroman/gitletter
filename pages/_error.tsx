69769e0f5e249396585c35488cb0cadf47bb5ac0
gdbroman <99gustaf@gmail.com>
2022-12-19 23:18:40 +0100

    Set up Sentry

---
+++ pages/_error.tsx
@@ -0,0 +1,19 @@
+import * as Sentry from "@sentry/nextjs";
+import NextErrorComponent from "next/error";
+import { NextPage } from "next/types";
+
+type Props = {
+  statusCode: number;
+};
+
+const CustomErrorPage: NextPage<Props> = ({ statusCode }) => {
+  return <NextErrorComponent statusCode={statusCode} />;
+};
+
+CustomErrorPage.getInitialProps = async (contextData) => {
+  await Sentry.captureUnderscoreErrorException(contextData);
+
+  return NextErrorComponent.getInitialProps(contextData);
+};
+
+export default CustomErrorPage;
